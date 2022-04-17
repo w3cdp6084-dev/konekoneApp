1a87fbbae42e35c240f01f8cf2bcbd1e6478e8f1
森裕輔 <yusukew3@gmail.com>
2022-04-17 13:11:47 +0900

    logo get

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.baseText}>
         <Text style={styles.mainTitle}>KONEKONE</Text>
+        <Image 
+          source={require('./assets/brand_logo.png')}
+          style={{ width: 200, height: 205 }}
+        />
         <StatusBar style="auto" />
       </Text>
     </View>
